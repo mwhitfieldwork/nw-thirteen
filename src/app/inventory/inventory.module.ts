18be63aa5a8cb614a090f8c6b16ff6b0aa05147a
mwhitfieldwork <mwhitfieldwork@gmail.com>
2024-12-02 19:31:53 -0600

    Add material to the select form

--- src/app/inventory/inventory.module.ts
+++ src/app/inventory/inventory.module.ts
@@ -6,7 +6,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { StockBranchComponent } from './stock-branch/stock-branch.component';
 import { StockSelectorComponent } from './stock-selector/stock-selector.component';
 import { StockProductsComponent } from './stock-products/stock-products.component';
-
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field'
+import {MatButtonModule} from '@angular/material/button';
 const routes:Routes =[
   {path: 'restock', component:InventoryComponent}
 ]
@@ -20,6 +23,10 @@ const routes:Routes =[
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
     RouterModule.forChild(routes),
   ]
 })
